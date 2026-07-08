2709c0356be3585c19d1c02528896260c9e9865c
Charm <mondjoe007@yahoo.com>
2026-07-08 15:50:57 +0400

    Update app.service.ts

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from './prisma.service';   // ← ADD THIS
 
 @Injectable()
 export class AppService {
+  constructor(private prisma: PrismaService) {}     // ← ADD THIS
+
   getHello(): string {
     return 'Hello World!';
+  }
+
+  getValidators() {
+    return this.prisma.validator.findMany();        // ← NOW THIS WORKS
   }
 }
