637a4c16398c438227d4d29528716b31d9dbb649
CharmCapsule <mondjoe007@yahoo.com>
2026-07-10 13:11:17 +0400

    Update solana.processor.ts

--- src/modules/solana/solana.processor.ts
+++ src/modules/solana/solana.processor.ts
@@ -1,16 +1,33 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { Worker } from 'bullmq';
 import { RedisService } from '../../cache/redis.service';
+import { SolanaService } from './solana.service';
 
 @Injectable()
 export class SolanaProcessor {
-  private worker: Worker;
+  private readonly logger = new Logger(SolanaProcessor.name);
 
-  constructor(private readonly redis: RedisService) {
-    this.worker = new Worker('solana', async job => {
-      // process job
-    }, {
-      connection: this.redis.getConnection(),
-    });
+  constructor(
+    private readonly redis: RedisService,
+    private readonly solana: SolanaService,
+  ) {
+    new Worker(
+      'solana-indexer',
+      async job => {
+        const { validatorAddress } = job.data;
+
+        switch (job.name) {
+          case 'index-slot':
+            await this.solana.indexValidatorSlot(validatorAddress);
+            break;
+          case 'index-balance':
+            await this.solana.indexValidatorBalance(validatorAddress);
+            break;
+          default:
+            this.logger.warn(`Unknown job: ${job.name}`);
+        }
+      },
+      { connection: this.redis.getConnection() },
+    );
   }
 }
