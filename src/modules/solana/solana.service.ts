67f16d82398dc822d9f844d0a272135bd7430357
CharmCapsule <mondjoe007@yahoo.com>
2026-07-10 13:06:55 +0400

    Update solana.service.ts

--- src/modules/solana/solana.service.ts
+++ src/modules/solana/solana.service.ts
@@ -1,11 +1,39 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { RpcManager } from '../../rpc/rpc.manager';
+import { PrismaService } from '../../prisma.service';
 
 @Injectable()
 export class SolanaService {
-  constructor(private readonly rpc: RpcManager) {}
+  private readonly logger = new Logger(SolanaService.name);
 
-  async getSlot() {
-    return this.rpc.call('getSlot', []);
+  constructor(
+    private readonly rpc: RpcManager,
+    private readonly prisma: PrismaService,
+  ) {}
+
+  async indexValidatorSlot(validatorAddress: string) {
+    const slot = await this.rpc.call<number>('getSlot');
+    this.logger.debug(`Slot: ${slot} for ${validatorAddress}`);
+
+    await this.prisma.validatorMetric.create({
+      data: {
+        validatorId: validatorAddress, // or mapped ID
+        slot,
+        value: 1,
+      },
+    });
+  }
+
+  async indexValidatorBalance(validatorAddress: string) {
+    const lamports = await this.rpc.call<number>('getBalance', [validatorAddress]);
+    const sol = lamports / 1_000_000_000;
+
+    await this.prisma.validatorMetric.create({
+      data: {
+        validatorId: validatorAddress,
+        slot: 0,
+        value: sol,
+      },
+    });
   }
 }
