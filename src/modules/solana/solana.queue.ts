77a85e79eb5f7cb5d6f04cbf1fbd015c2161fe8b
CharmCapsule <mondjoe007@yahoo.com>
2026-07-10 13:10:39 +0400

    Update solana.queue.ts

--- src/modules/solana/solana.queue.ts
+++ src/modules/solana/solana.queue.ts
@@ -2,13 +2,23 @@ import { Injectable } from '@nestjs/common';
 import { Queue } from 'bullmq';
 import { RedisService } from '../../cache/redis.service';
 
+export type SolanaJobName = 'index-slot' | 'index-balance';
+
 @Injectable()
 export class SolanaQueue {
   private queue: Queue;
 
   constructor(private readonly redis: RedisService) {
-    this.queue = new Queue('solana', {
+    this.queue = new Queue('solana-indexer', {
       connection: this.redis.getConnection(),
     });
+  }
+
+  async enqueueSlotIndex(validatorAddress: string) {
+    await this.queue.add('index-slot', { validatorAddress });
+  }
+
+  async enqueueBalanceIndex(validatorAddress: string) {
+    await this.queue.add('index-balance', { validatorAddress });
   }
 }
