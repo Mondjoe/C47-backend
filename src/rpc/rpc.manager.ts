382babb796bbfda670f2b8a65b975bc5778440fe
CharmCapsule <mondjoe007@yahoo.com>
2026-07-10 13:04:55 +0400

    Update rpc.manager.ts

--- src/rpc/rpc.manager.ts
+++ src/rpc/rpc.manager.ts
@@ -3,14 +3,18 @@ import axios from 'axios';
 export class RpcManager {
   constructor(private readonly url: string) {}
 
-  async call(method: string, params: any[]) {
-    const response = await axios.post(this.url, {
+  async call<T = any>(method: string, params: any[] = []): Promise<T> {
+    const { data } = await axios.post(this.url, {
       jsonrpc: '2.0',
       id: 1,
       method,
       params,
     });
 
-    return response.data.result;
+    if (data.error) {
+      throw new Error(data.error.message);
+    }
+
+    return data.result as T;
   }
 }
