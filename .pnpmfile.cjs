578a92e18cdc4fcf5d5df980550f404b993bddc4
CharmCapsule <kyawzayya4788@gmail.com>
2026-07-20 15:55:48 +0400

    Delete .pnpmfile.cjs

--- .pnpmfile.cjs
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  hooks: {
-    readPackage(pkg) {
-      pkg.scripts = pkg.scripts || {};
-      pkg.scripts.prepare = pkg.scripts.prepare || "";
-      return pkg;
-    }
-  }
-};
