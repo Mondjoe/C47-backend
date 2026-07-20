6812ab5e9796ec55c178ce199033e8950bfd2843
CharmCapsule <kyawzayya4788@gmail.com>
2026-07-20 22:52:43 +0400

    Delete app.service.d.ts

--- dist/app.service.d.ts
+++
@@ -1,7 +0,0 @@
-import { PrismaService } from './prisma.service';
-export declare class AppService {
-    private prisma;
-    constructor(prisma: PrismaService);
-    getHello(): string;
-    getValidators(): any;
-}
