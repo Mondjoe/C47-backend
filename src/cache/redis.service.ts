e1078c1682b51b2a0d9ae2ec1d4a081dd9199669
CharmCapsule <kyawzayya4788@gmail.com>
2026-07-27 12:08:39 +0400

    Update redis.service.ts

--- src/cache/redis.service.ts
+++ src/cache/redis.service.ts
@@ -1,18 +1,11 @@
 import { Injectable } from '@nestjs/common';
-import { createClient, RedisClientType } from 'redis';
 
 @Injectable()
 export class RedisService {
-  private client: RedisClientType;
-
-  constructor() {
-    this.client = createClient({
-      url: process.env.REDIS_URL,
-    });
-    this.client.connect();
-  }
-
   getConnection() {
-    return this.client;
+    return {
+      host: process.env.REDIS_HOST || '127.0.0.1',
+      port: Number(process.env.REDIS_PORT) || 6379,
+    };
   }
 }
