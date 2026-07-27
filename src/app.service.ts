8fcab1d32d9023a3ff4ea8737e0ea317f9eaa807
CharmCapsule <kyawzayya4788@gmail.com>
2026-07-27 21:20:35 +0400

    Update app.service.ts

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { PrismaService } from './prisma.service';   // ← ADD THIS
+import { PrismaService } from './database/prisma/prisma.service';
 
 @Injectable()
 export class AppService {
